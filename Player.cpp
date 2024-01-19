1286133e1eaa1c43b86a83504450fcbe8005239f
武田一護 <220256@jc-21.jp>
2024-01-19 11:16:59 +0900

    いらないやつ消した

--- Player.cpp
+++ Player.cpp
@@ -103,64 +103,7 @@ void Player::Update()
 
     }
 
-	//壁との判定
-	//int checkX1, checkX2;
-	//int checkZ1, checkZ2;
-	////右
-	//{
-	//	checkX1 = (int)(transform_.position_.x + 0.3f);
-	//	checkZ1 = (int)(transform_.position_.z + 0.2f);
-
-	//	checkX2 = (int)(transform_.position_.x + 0.3f);
-	//	checkZ2 = (int)(transform_.position_.z - 0.2f);
-	//	if (pStage_->IsWall(checkX1, checkZ1) == true || pStage_->IsWall(checkX2, checkZ2) == true)
-	//	{
-	//		transform_.position_.x = (float)((int)prevPosition_.x) + 1.0f - 0.3;
-	//	}
-	//}
-
-	////左
-	//{
-	//	checkX1 = (int)(transform_.position_.x - 0.3f);
-	//	checkZ1 = (int)(transform_.position_.z + 0.1f);
-	//	checkX2 = (int)(transform_.position_.x - 0.3f);
-	//	checkZ2 = (int)(transform_.position_.z - 0.1f);
-
-	//	if (pStage_->IsWall(checkX1, checkZ1) == true || pStage_->IsWall(checkX2, checkZ2) == true)
-	//	{
-	//		transform_.position_.x = (float)((int)prevPosition_.x) + 0.3;
-	//	}
-
-	//}
-
-	////前
-	//{
-	//	checkX1 = (int)(transform_.position_.x + 0.1f);
-	//	checkZ1 = (int)(transform_.position_.z + 0.3f);
-	//	checkX2 = (int)(transform_.position_.x - 0.1f);
-	//	checkZ2 = (int)(transform_.position_.z + 0.3f);
-
-	//	if (pStage_->IsWall(checkX1, checkZ1) == true ||pStage_->IsWall(checkX2, checkZ2) == true)
-	//	{
-	//		transform_.position_.z = (float)((int)prevPosition_.z) + 1.0f - 0.3;
-	//	}
-	//}
-
-	////後
-	//{
-	//	checkX1 = (int)(transform_.position_.x + 0.1f);
-	//	checkZ1 = (int)(transform_.position_.z - 0.3f);
-	//	checkX2 = (int)(transform_.position_.x - 0.1f);
-	//	checkZ2 = (int)(transform_.position_.z - 0.3f);
-
-	//	if (pStage_->IsWall(checkX1, checkZ1) == true ||pStage_->IsWall(checkX2, checkZ2) == true)
-	//	{
-	//		transform_.position_.z = (float)((int)prevPosition_.z) + 0.3;
-	//	}
-	//}
-
-	//transform_.rotate_.y = XMConvertToDegrees(angle);
-
+	
 	
     
 }
@@ -180,18 +123,6 @@ void Player::Release()
 {
 }
 
-//カメラ位置(一人称)
-//void Player::CameraPosition()
-//{
-//    //1フレームの移動ベクトル
-//   // XMVECTOR vMove{ 0.0f,0.0f,0.1f,0.0f };//奥に0.1ｍ
-//    //オブジェクトの現在地をベクトル型に変換
-//    XMVECTOR vPos = XMLoadFloat3(&transform_.position_);
-//    Camera::SetPosition(transform_.position_);
-//    XMFLOAT3 camTarget;
-//    XMStoreFloat3(&camTarget, vPos + vMove);
-//    Camera::SetTarget(camTarget);
-//}
 
 
 

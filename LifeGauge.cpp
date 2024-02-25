7c9af3cc812dc09ea5645166718cc296a7b498b2
一護 武田 <220256@jc-21.jp>
2024-02-26 04:01:28 +0900

    ゲージもろ追加

--- LifeGauge.cpp
+++ LifeGauge.cpp
@@ -1,1 +1,38 @@
 #include "LifeGauge.h"
+#include "Engine/Image.h"
+
+//コンストラクタ
+LifeGauge::LifeGauge(GameObject* parent)
+    :GameObject(parent, "LifeGauge"), hPict_(-1)
+{
+}
+
+//デストラクタ
+LifeGauge::~LifeGauge()
+{
+}
+
+//初期化
+void LifeGauge::Initialize()
+{
+    //画像データのロード
+    hPict_ = Image::Load("Frame.png");
+    assert(hPict_ >= 0);
+}
+
+//更新
+void LifeGauge::Update()
+{
+}
+
+//描画
+void LifeGauge::Draw()
+{
+    Image::SetTransform(hPict_, transform_);
+    Image::Draw(hPict_);
+}
+
+//開放
+void LifeGauge::Release()
+{
+}
--- LifeGauge.h
+++ LifeGauge.h
@@ -1,5 +1,29 @@
 #pragma once
-class LifeGauge
+#include "Engine/GameObject.h"
+
+//◆◆◆を管理するクラス
+class LifeGauge : public GameObject
 {
+private:
+    int hPict_;    //画像番号
+
+public:
+    //コンストラクタ
+    LifeGauge(GameObject* parent);
+
+    //デストラクタ
+    ~LifeGauge();
+
+    //初期化
+    void Initialize() override;
+
+    //更新
+    void Update() override;
+
+    //描画
+    void Draw() override;
+
+    //開放
+    void Release() override;
 };
 

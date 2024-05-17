ea5a4e9b80b5e6c56a2b164332802af7552c167d
一護 武田 <220256@jc-21.jp>
2024-05-17 14:23:34 +0900

    サウンド骨組みだけ

--- Sound.cpp
+++ Sound.cpp
@@ -7,5 +7,13 @@ void Sound::InitializeX3DAudio()
 
 void Sound::CalculateSoundPosition(Listener& listener, SoundEmitter* emitters, int numEmitters)
 {
-
+    //X3DAudioオブジェクトの作成
+    X3DAUDIO_HANDLE x3DInstance;
+    X3DAUdioInitialize(D3D_AUDIO_TO_PROCESSOR, X3DAUDIO_SPEED_OF_SOUND, x3DInstance);
+    //リスナー情報の設定
+    X3DAudioCalculate(x3DInstance, &listener.listener, emitters[0].emitter, X3DAUDIO_CALCULATE_MATRIX);
+    //各音源の配置計算
+    for (int i = 0; i < numEmitters; i++) {
+        X3DAudioCalculate(x3DInstance, &listener.listener, emitters[i].emitter, X3DAUDIO_CALCULATE_DSP_SETTINGS);
+     }
 }

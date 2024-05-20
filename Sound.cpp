8c938feacdb0b0d2a7b776921a9e8d1b54106a07
一護 武田 <220256@jc-21.jp>
2024-05-20 10:07:23 +0900

    サウンドに関数設定

--- Sound.cpp
+++ Sound.cpp
@@ -1,19 +1,2 @@
 #include "Sound.h"
 
-void Sound::InitializeX3DAudio()
-{
-	//X3DAudioの初期化処理を追加
-}
-
-void Sound::CalculateSoundPosition(Listener& listener, SoundEmitter* emitters, int numEmitters)
-{
-    //X3DAudioオブジェクトの作成
-    X3DAUDIO_HANDLE x3DInstance{};
-   /* X3DAUdioInitialize(D3D_AUDIO_TO_PROCESSOR, X3DAUDIO_SPEED_OF_SOUND, x3DInstance);
-    //リスナー情報の設定
-    X3DAudioCalculate(x3DInstance, &listener.listener, emitters[0].emitter, X3DAUDIO_CALCULATE_MATRIX);
-    //各音源の配置計算
-    for (int i = 0; i < numEmitters; i++) {
-        X3DAudioCalculate(x3DInstance, &listener.listener, emitters[i].emitter, X3DAUDIO_CALCULATE_DSP_SETTINGS);
-     }*/
-}
--- Sound.h
+++ Sound.h
@@ -1,38 +1,22 @@
 #pragma once
-#include <x3daudio.h>
+#include<Windows.h>
+#include<X3DAudio.h>
+#include<xaudio2.h>
 
-//エミッター情報の構造体
-struct SoundEmitter{
-	//X3DAudioのエミッター構造体
-	X3DAUDIO_EMITTER emitter;
-
-	//X3DAudioのDSP設定構造体
-	X3DAUDIO_DSP_SETTINGS dspSettings;
-
-private:
-	float volume;        //音量
-	float minDistance;  //最小距離
-	float maxDistance;  //最大距離
-	
-};
-
-//リスナー情報の構造体
-struct Listener{
-
-	//X3DAudioのリスナー構造体
-	X3DAUDIO_LISTENER listener;
-
-	//その他追加予定
-
-};
 
 class Sound
 {
-	//X3DAudioの初期化
-	void  InitializeX3DAudio();
 
-	//音源配置の計算
-	void CalculateSoundPosition(Listener& listener, SoundEmitter* emitters, int numEmitters);
+	bool InitializeXAudio2(IXAudio2** ppXAudio2, IXAudio2MasteringVoice** ppMasteringVoice);
+
+	//X3DAudioの初期化
+	bool Initialize3DAudio(IXAudio2MasteringVoice* pMasteringVoice, X3DAUDIO_HANDLE& x3DInstance);
+
+	//エミッター設定
+	void SetupEmitter(X3DAUDIO_EMITTER& emitter);
+
+	//リスナーの設定
+	void SetupListener(X3DAUDIO_LISTENER& listener);
 
 };
 

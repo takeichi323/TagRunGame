70f4f4fb6679007d19ac76fad7c1806237bf2725
一護 武田 <220256@jc-21.jp>
2024-05-22 16:35:05 +0900

    sound系の初期化を書き込んだ

--- Sound.cpp
+++ Sound.cpp
@@ -1,2 +1,14 @@
 #include "Sound.h"
 
+bool Sound::InitializeXAudio2(IXAudio2** ppXAudio2, IXAudio2MasteringVoice** ppMasterVoice)
+{
+	HRESULT hr{};
+
+	hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	return false;
+}
+
+bool Sound::InitializeX3DAudio(IXAudio2MasteringVoice* pMasterVoice, X3DAUDIO_HANDLE& x3DInstance)
+{
+	return false;
+}
--- Sound.h
+++ Sound.h
@@ -3,20 +3,20 @@
 #include<X3DAudio.h>
 #include<xaudio2.h>
 
+//ライブラリ読み込み
+#pragma comment(lib, "XAudio2.lib")
+#pragma comment(lib, "X3DAudio.lib")
 
 class Sound
 {
 
-	bool InitializeXAudio2(IXAudio2** ppXAudio2, IXAudio2MasteringVoice** ppMasteringVoice);
+  //XAudio2の初期化
+   bool InitializeXAudio2(IXAudio2** ppXAudio2, IXAudio2MasteringVoice** ppMasterVoice);
 
-	//X3DAudioの初期化
-	bool Initialize3DAudio(IXAudio2MasteringVoice* pMasteringVoice, X3DAUDIO_HANDLE& x3DInstance);
+   //X3DAudioの初期化
+   bool InitializeX3DAudio(IXAudio2MasteringVoice* pMasterVoice, X3DAUDIO_HANDLE& x3DInstance);
 
-	//エミッター設定
-	void SetupEmitter(X3DAUDIO_EMITTER& emitter);
 
-	//リスナーの設定
-	void SetupListener(X3DAUDIO_LISTENER& listener);
 
 };
 
